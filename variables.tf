dd834f8d0b2582c62565876c96f5e105d77f58fc
EC2 Default User <ec2-user@ip-172-31-93-150.ec2.internal>
2021-11-04 23:03:09 +0000

    first commit

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "sc_host" {}
+variable "sc_username" {}
+variable "sc_password" {}
+variable "sc_project_name" {}
+
+variable "cluster_cloud_account_name" {}
+variable "cluster_cluster_profile_name" {}
+variable "cluster_name" {}
+variable "ssh_key_name" {}
+variable "region" {}
+variable "region_azs" {}
